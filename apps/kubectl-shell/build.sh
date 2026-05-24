4b8d32c18bb4c3f6b0771e1fc176ac025b55a2a8
Freek <freek.george.hoekstra@gmail.com>
2026-05-24 23:01:31 +0200

    Bump tag

--- apps/kubectl-shell/build.sh
+++ apps/kubectl-shell/build.sh
@@ -3,4 +3,4 @@ sudo docker buildx create --name multiarch --driver docker-container --use
 sudo docker buildx inspect --bootstrap
 
 echo $CR_PAT | sudo docker login ghcr.io -u fhoekstra --password-stdin
-sudo docker buildx build . --file ./Containerfile --platform linux/amd64,linux/arm64 -t ghcr.io/fhoekstra/kubectl-shell:v1.36.1 --push
+sudo docker buildx build . --file ./Containerfile --platform linux/amd64,linux/arm64 -t ghcr.io/fhoekstra/kubectl-shell:v1.36.1-1 --push
